153f9d127bccb606e7c1ebc37c564937375ff3cb
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2019-10-07 14:00:31 -0700

    added isoalte heap statistics

--- isolate.go
+++ isolate.go
@@ -17,6 +17,21 @@ type Isolate struct {
 	ptr C.IsolatePtr
 }
 
+// HeapStatistics represents V8 isolate heap statistics
+type HeapStatistics struct {
+	TotalHeapSize            uint64
+	TotalHeapSizeExecutable  uint64
+	TotalPhysicalSize        uint64
+	TotalAvailableSize       uint64
+	UsedHeapSize             uint64
+	HeapSizeLimit            uint64
+	MallocedMemory           uint64
+	ExternalMemory           uint64
+	PeakMallocedMemory       uint64
+	NumberOfNativeContexts   uint64
+	NumberOfDetachedContexts uint64
+}
+
 // NewIsolate creates a new V8 isolate. Only one thread may access
 // a given isolate at a time, but different threads may access
 // different isolates simultaneously.
@@ -34,6 +49,25 @@ func NewIsolate() (*Isolate, error) {
 // of JavaScript execution in the given isolate.
 func (i *Isolate) TerminateExecution() {
 	C.IsolateTerminateExecution(i.ptr)
+}
+
+// GetHeapStatistics returns heap statistics for an isolate.
+func (i *Isolate) GetHeapStatistics() HeapStatistics {
+	hs := C.IsolationGetHeapStatistics(i.ptr)
+
+	return HeapStatistics{
+		TotalHeapSize:            uint64(hs.total_heap_size),
+		TotalHeapSizeExecutable:  uint64(hs.total_heap_size_executable),
+		TotalPhysicalSize:        uint64(hs.total_physical_size),
+		TotalAvailableSize:       uint64(hs.total_available_size),
+		UsedHeapSize:             uint64(hs.used_heap_size),
+		HeapSizeLimit:            uint64(hs.heap_size_limit),
+		MallocedMemory:           uint64(hs.malloced_memory),
+		ExternalMemory:           uint64(hs.external_memory),
+		PeakMallocedMemory:       uint64(hs.peak_malloced_memory),
+		NumberOfNativeContexts:   uint64(hs.number_of_native_contexts),
+		NumberOfDetachedContexts: uint64(hs.number_of_detached_contexts),
+	}
 }
 
 func (i *Isolate) finalizer() {
