b4162ebf5f88aada98e67360ab362b3f8a465e0a
neptoess <neptoess@gmail.com>
2020-12-17 18:20:08 -0500

    Use correct syntax for #cgo constraints

--- cgo.go
+++ cgo.go
@@ -1,7 +1,7 @@
 package v8go
 
 // #cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
-// #cgo (darwin OR linux) CXXFLAGS: -I${SRCDIR}/deps/include
+// #cgo darwin linux CXXFLAGS: -I${SRCDIR}/deps/include
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo windows LDFLAGS: -lv8_libplatform
 // #cgo darwin LDFLAGS: -L${SRCDIR}/deps/darwin-x86_64
