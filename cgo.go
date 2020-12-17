4ad0dfad2429850d1c49a61f2c491349e2a3973d
neptoess <neptoess@gmail.com>
2020-12-17 17:49:21 -0500

    Switch up #cgo flags to match master

--- cgo.go
+++ cgo.go
@@ -1,8 +1,7 @@
 package v8go
 
-// #cgo CXXFLAGS: -fno-rtti -fpic -std=c++14
-// #cgo !windows CXXFLAGS: -I${SRCDIR}/deps/include
-// #cgo windows CXXFLAGS: -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
+// #cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
+// #cgo (darwin OR linux) CXXFLAGS: -I${SRCDIR}/deps/include
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo windows LDFLAGS: -lv8_libplatform
 // #cgo darwin LDFLAGS: -L${SRCDIR}/deps/darwin-x86_64
