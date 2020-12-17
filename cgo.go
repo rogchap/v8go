52e7bbe59ee4557cf33c6ac3377ea6b9acab73ba
Roger Chapman <rogchap@gmail.com>
2020-12-18 09:40:07 +1100

    add pointer compression

--- cgo.go
+++ cgo.go
@@ -1,6 +1,6 @@
 package v8go
 
-// #cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -I${SRCDIR}/deps/include
+// #cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -I${SRCDIR}/deps/include -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin LDFLAGS: -L${SRCDIR}/deps/darwin-x86_64
 // #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux-x86_64
