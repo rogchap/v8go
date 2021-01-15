97036754b0810f9381f8dffb578e8214c272a2f3
Roger Chapman <rogchap@gmail.com>
2021-01-15 21:07:13 +1100

    debug Go 1.12.7

--- value_test.go
+++ value_test.go
@@ -319,7 +319,7 @@ func TestValueBigInt(t *testing.T) {
 	}{
 		{"BigInt(0)", &big.Int{}},
 		{"-1n", big.NewInt(-1)},
-		{"new BigInt(1)", nil}, // bad syntax
+		// {"new BigInt(1)", nil}, // bad syntax
 		{"BigInt(Number.MAX_SAFE_INTEGER)", big.NewInt(1<<53 - 1)},
 		{"BigInt(Number.MIN_SAFE_INTEGER)", new(big.Int).Neg(big.NewInt(1<<53 - 1))},
 		{"BigInt(Number.MAX_SAFE_INTEGER) * 2n", big.NewInt(1<<54 - 2)},
@@ -333,6 +333,8 @@ func TestValueBigInt(t *testing.T) {
 			ctx, _ := v8go.NewContext(iso)
 			val, _ := ctx.RunScript(tt.source, "test.js")
 			b := val.BigInt()
+			fmt.Printf("b = %+v\n", b)
+			fmt.Printf("tt = %+v\n", tt)
 			if b == nil && tt.expected != nil {
 				t.Errorf("uexpected <nil> value")
 				return
