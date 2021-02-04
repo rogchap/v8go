bcb05a0d20fd281482647a577dc7dbbec7c0d5e9
Roger Chapman <rogchap@gmail.com>
2021-02-04 15:02:52 +1100

    BSD-style licence

--- v8go_test.go
+++ v8go_test.go
@@ -1,3 +1,7 @@
+// Copyright 2019 Roger Chapman and the v8go contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package v8go_test
 
 import (
