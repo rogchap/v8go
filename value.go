5e6a790ae9dd02866e73f7d1ddff7f556359429c
Richard Hutta <huttarichard@gmail.com>
2021-07-07 20:34:10 +0200

    Values list

--- value.go
+++ value.go
@@ -561,3 +561,15 @@ func (v *Value) MarshalJSON() ([]byte, error) {
 	}
 	return []byte(jsonStr), nil
 }
+
+// Values as a list of v8go Values.
+type Values []*Value
+
+// ValuesToValuer converts actual value to Valuer interface.
+func (vv Values) Valuers() []Valuer {
+	xx := make([]Valuer, len(vv))
+	for _, v := range vv {
+		xx = append(xx, v)
+	}
+	return xx
+}
