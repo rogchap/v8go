169a89f40cf1cd4a1ad12e662dcd65cb8e0da1e1
Genevieve L'Esperance <genevieve.lesperance@shopify.com>
2021-07-22 17:05:49 -0700

    Support for fn source map url
    PR: https://github.com/Shopify/v8go/pull/8

--- v8go.h
+++ v8go.h
@@ -181,6 +181,7 @@ extern ValuePtr PromiseResult(ValuePtr ptr);
 
 extern RtnValue FunctionCall(ValuePtr ptr, int argc, ValuePtr argv[]);
 RtnValue FunctionNewInstance(ValuePtr ptr, int argc, ValuePtr args[]);
+ValuePtr FunctionSourceMapUrl(ValuePtr ptr);
 
 extern ValuePtr ExceptionError(IsolatePtr iso_ptr, const char* message);
 extern ValuePtr ExceptionRangeError(IsolatePtr iso_ptr, const char* message);
