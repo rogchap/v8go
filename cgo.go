c42a07aa18afc6def4afc669418a409b7b76fc9d
Colin King <me@colinking.co>
2021-08-26 16:09:17 -0400

    cgo

--- cgo.go
+++ cgo.go
@@ -10,7 +10,8 @@ package v8go
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
-// #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux_x86_64
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/linux_x86_64
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/linux_arm64
 // #cgo windows LDFLAGS: -L${SRCDIR}/deps/windows_x86_64 -static -ldbghelp -lssp -lwinmm -lz
 import "C"
 
