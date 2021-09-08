4de6124ec25ecd88750974ce9f465ac61ffa92fc
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2021-09-08 11:22:01 -0400

    Use Function.Call to implement Object.MethodCall

--- object.go
+++ object.go
@@ -32,19 +32,7 @@ func (o *Object) MethodCall(methodName string, args ...Valuer) (*Value, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	var argptr *C.ValuePtr
-	if len(args) > 0 {
-		var cArgs = make([]C.ValuePtr, len(args))
-		for i, arg := range args {
-			cArgs[i] = arg.value().ptr
-		}
-		argptr = (*C.ValuePtr)(unsafe.Pointer(&cArgs[0]))
-	}
-	fn.ctx.register()
-	rtn := C.FunctionCall(fn.ptr, o.ptr, C.int(len(args)), argptr)
-	fn.ctx.deregister()
-	return getValue(fn.ctx, rtn), getError(rtn)
+	return fn.Call(o, args...)
 }
 
 // Set will set a property on the Object to a given value.
