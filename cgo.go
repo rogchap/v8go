4f3bfd66be288d7484bf32ae31daae637a703659
Colin King <me@colinking.co>
2021-09-28 17:37:06 -0400

    Add support for `arm64` on Linux (#15)

--- cgo.go
+++ cgo.go
@@ -10,7 +10,8 @@ package v8go
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
-// #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux_x86_64
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/linux_x86_64
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/linux_arm64
 // #cgo windows LDFLAGS: -L${SRCDIR}/deps/windows_x86_64 -static -ldbghelp -lssp -lwinmm -lz
 import "C"
 
@@ -23,6 +24,7 @@ import (
 	_ "github.com/airplanedev/v8go/deps/include"
 	_ "github.com/airplanedev/v8go/deps/include/cppgc"
 	_ "github.com/airplanedev/v8go/deps/include/libplatform"
+	_ "github.com/airplanedev/v8go/deps/linux_arm64"
 	_ "github.com/airplanedev/v8go/deps/linux_x86_64"
 	_ "github.com/airplanedev/v8go/deps/windows_x86_64"
 )
