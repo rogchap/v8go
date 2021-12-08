1dba3adcee50e077a7b676e72f73c02ec2e611a4
Genevieve L'Esperance <genevieve.lesperance@shopify.com>
2021-12-08 13:06:40 -0800

    Add -ldl flag to linux build to fix missing dlsym errors

--- cgo.go
+++ cgo.go
@@ -10,7 +10,7 @@ package v8go
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
-// #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux_x86_64
+// #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux_x86_64 -ldl
 import "C"
 
 // These imports forces `go mod vendor` to pull in all the folders that
