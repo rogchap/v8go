943fcf9e74ccd92e4d4816e77b47ee9bbba709bd
Aditya Sharma <git@adi.run>
2021-12-22 09:30:54 -0800

    Build v8 for linux_arm64 (#223)
    * Build v8 for linux_arm64
    
    * Update V8 static library for ubuntu-18.04 arm64
    
    Co-authored-by: epk <epk@users.noreply.github.com>

--- cgo.go
+++ cgo.go
@@ -10,7 +10,8 @@ package v8go
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
-// #cgo linux LDFLAGS: -L${SRCDIR}/deps/linux_x86_64 -ldl
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/linux_x86_64 -ldl
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/linux_arm64 -ldl
 import "C"
 
 // These imports forces `go mod vendor` to pull in all the folders that
@@ -20,5 +21,6 @@ import (
 	_ "rogchap.com/v8go/deps/darwin_arm64"
 	_ "rogchap.com/v8go/deps/darwin_x86_64"
 	_ "rogchap.com/v8go/deps/include"
+	_ "rogchap.com/v8go/deps/linux_arm64"
 	_ "rogchap.com/v8go/deps/linux_x86_64"
 )
